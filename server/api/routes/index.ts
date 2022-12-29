82e34a290c4d505c1469b4f111e8a2a95347ba00
tarcizo <tarcizon12@gmail.com>
2022-12-28 23:14:51 -0300

    Recebendo parametros da query request

--- server/api/routes/index.ts
+++ server/api/routes/index.ts
@@ -3,11 +3,27 @@ import { getCoinAction}  from '../../actions/coinsAction'
 
 const router = Router();
 
-router.get('/',(request, response)=>{
-    getCoinAction().then((list: any)=>{
-        response.send(`lista: ${list}`);
-    });
-    
+router.get('/',async (request, response)=>{
+    try {
+        //Recebendo parametro da requisicao 
+        //url = localhost:3003/?coin='rubro'        
+        const listCoins = await getCoinAction();
+        const nameCoin = request.query.coin;    
+
+        if(listCoins){
+            if(nameCoin){
+                const filteredElements = listCoins.filter(element => element.coin == nameCoin);
+                response.json(filteredElements)
+            }else{
+                response.json(listCoins)
+            }
+        }else{
+            console.log("Erro ao receber lista na requisicao get");
+        }
+        
+    } catch (error) {
+        console.log(`Erro ao solicitar informacoes: ${error}`);
+    }
 });
 
 export { router } 
